7504a4fadc70cecdaf5d96906117df35f93b6640
Rafael <rafael.f.duarte.2007@gmail.com>
2024-12-20 11:24:24 +0000

    Operaçôes Básicas Python

---
+++ OperacoesBasicas.py
@@ -0,0 +1,26 @@
+'''
+    Programador..:Rafael Duarte
+    Data.........:20/12/2024
+    Observações..:Operações básicas em Python.
+'''
+
+n1 = 0
+n2 = 0
+soma = 0
+subtracao = 0
+multiplicacao = 0 
+divisao = 0
+
+n1 = int(input("Introduza um número inteiro: "))
+n2 = int(input("Introduza outro número inteiro: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print("Soma: ", soma)
+print("Subtração:", subtracao)
+print("Multiplicação: ", multiplicacao)
+print("Divisão: ", divisao)
+
